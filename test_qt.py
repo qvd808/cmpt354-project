8f73b2cd909e93fcfddde04c22275331f50203c7
Quang Vinh Dang <dqvinh808@gmail.com>
2023-12-02 20:21:46 -0800

    Making application quit if connection to database fails

--- test_qt.py
+++ test_qt.py
@@ -1,45 +1,29 @@
 from test_design import Ui_MainWindow
-from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6 import QtWidgets
 from tools.functionality import Functionality
 from tools.functionality import SEARCH_BUSINESS_FILTER, SEARCH_BUSINESS_ORDER, SEARCH_USER_YELP
 import pypyodbc
 from dotenv import dotenv_values
-from PyQt6.QtWidgets import QLabel, QVBoxLayout, QDialog, QProgressDialog
-from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
-from PyQt6.QtCore import Qt, QTimer
+class WorkerThread(QThread):
+    finished_signal = pyqtSignal(bool)
 
-class LoadingScreen(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def run(self):
+        # Simulate some time-consuming work
+        from dotenv import dotenv_values
+        config = dotenv_values(".env")  # take environment variables from .env.
 
-        # Set up the main window
-        self.setWindowTitle("Loading Screen")
-        self.setGeometry(100, 100, 400, 200)
-
-        # Create a central widget and set it as the main window's central widget
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-
-        # Create a vertical layout for the central widget
-        layout = QVBoxLayout(central_widget)
-
-        # Add a label for the loading message
-        self.loading_label = QLabel("Loading...", self)
-        layout.addWidget(self.loading_label)
-
-        # Create a timer to simulate loading (you can replace this with your actual loading logic)
-        self.loading_timer = QTimer(self)
-        self.loading_timer.timeout.connect(self.update_loading_message)
-        self.loading_timer.start(500)  # Update every 500 milliseconds
-
-    def update_loading_message(self):
-        # Update the loading message
-        current_text = self.loading_label.text()
-        dots = current_text.count('.')
-        dots = (dots + 1) % 4
-        self.loading_label.setText("Loading" + "." * dots)
+        db_host = config['DB_HOST']
+        db_name = config['DB_NAME']
+        db_user = config['DB_USER']
+        db_password = config['DB_PASSWORD']
+        try:
+            connection_string = 'Driver={SQL Server};Server=' + db_host + ';Database=' + db_name + ';UID=' + db_user + ';PWD=' + db_password + ';'
+            connection = pypyodbc.connect(connection_string)
+            self.finished_signal.emit(True)
+        except Exception as e:
+            self.finished_signal.emit(False)
 
 
 class Application(Ui_MainWindow):
@@ -63,6 +47,23 @@ class Application(Ui_MainWindow):
         # except pypyodbc.DatabaseError:
         #     print("Unable to connect")
         self.pushButton.clicked.connect(self.sign_in_function)
+        self.worker_thread = None
+        self.run_on_startup()
+
+    def run_on_startup(self):
+        if self.worker_thread is None or not self.worker_thread.isRunning():
+            self.worker_thread = WorkerThread()
+            self.worker_thread.finished_signal.connect(self.handle_thread_result)
+            self.worker_thread.start()
+
+    def handle_thread_result(self, connection_successful):
+        if connection_successful:
+            print("Connection successful.")
+            self.terminate_button.hide()
+            self.result_label.setText("Connection successful.")
+        else:
+            print("Connection failed. Terminating application.")
+            app.quit()
 
     def connec_database(self):
             self.controller.setCursor(self.controller.init_connection(
@@ -92,12 +93,7 @@ if __name__ == "__main__":
 
     app = QtWidgets.QApplication(sys.argv)
     controller = Functionality()
-    controller.setCursor(controller.init_connection(
-        db_host=db_host,
-        db_name=db_name,
-        db_password=db_password,
-        db_user=db_user
-    ))
+
     MainWindow = QtWidgets.QMainWindow()
     ui = Application(
         MainWindow,
