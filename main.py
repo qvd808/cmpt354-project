8d0ba1a49a88544ba41ed2e9f0c0465ba04b93fb
Quang Vinh Dang <dqvinh808@gmail.com>
2023-12-02 20:15:40 -0800

    Update with thread running checking for connection

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import sys
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QLabel
+import pypyodbc
 
 class WorkerThread(QThread):
-    finished_signal = pyqtSignal()
+    finished_signal = pyqtSignal(bool)
 
     def run(self):
         # Simulate some time-consuming work
-        self.msleep(3000)
-        self.finished_signal.emit()
+        from dotenv import dotenv_values
+        config = dotenv_values(".env")  # take environment variables from .env.
+
+        db_host = config['DB_HOST']
+        db_name = config['DB_NAME']
+        db_user = config['DB_USER']
+        db_password = config['DB_PASSWORD']
+        try:
+            connection_string = 'Driver={SQL Server};Server=' + db_host + ';Database=' + db_name + ';UID=' + db_user + ';PWD=' + db_password + ';'
+            connection = pypyodbc.connect(connection_string)
+            self.finished_signal.emit(True)
+        except Exception as e:
+            self.finished_signal.emit(False)
 
 class MyMainWindow(QMainWindow):
     def __init__(self):
@@ -25,17 +37,25 @@ class MyMainWindow(QMainWindow):
         self.terminate_button.clicked.connect(self.create_thread)
         layout.addWidget(self.terminate_button)
 
+        self.result_label = QLabel(self)
+        layout.addWidget(self.result_label)
+
         self.worker_thread = None
 
     def create_thread(self):
         if self.worker_thread is None or not self.worker_thread.isRunning():
             self.worker_thread = WorkerThread()
-            self.worker_thread.finished_signal.connect(self.terminate_application)
+            self.worker_thread.finished_signal.connect(self.handle_thread_result)
             self.worker_thread.start()
 
-    def terminate_application(self):
-        print("Thread finished. Terminating application.")
-        app.quit()
+    def handle_thread_result(self, connection_successful):
+        if connection_successful:
+            print("Connection successful.")
+            self.terminate_button.hide()
+            self.result_label.setText("Connection successful.")
+        else:
+            print("Connection failed. Terminating application.")
+            app.quit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
