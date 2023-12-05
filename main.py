989976725e727c0d7642d8160c221eea41897f84
Quang Vinh Dang <dqvinh808@gmail.com>
2023-12-04 19:58:03 -0800

    Final file

--- main.py
+++ main.py
@@ -1,12 +1,252 @@
-from test_login_screen import Ui_LoginScreen
 from PyQt6 import QtWidgets, QtCore
-from tools.functionality import Functionality
-from tools.functionality import SEARCH_BUSINESS_FILTER, SEARCH_BUSINESS_ORDER, SEARCH_USER_YELP
 import pypyodbc, sys
 from PyQt6.QtCore import QThread, pyqtSignal, QRect
-from PyQt6.QtWidgets import QMessageBox, QProgressBar,QVBoxLayout, QDialog, QWidget,QTableWidget, QTableWidgetItem,QHeaderView
-from PyQt6.QtWidgets import QMenu
+from PyQt6.QtWidgets import QMessageBox, QProgressBar,QVBoxLayout
+from PyQt6.QtWidgets import QMenu, QDialog, QWidget,QTableWidget, QTableWidgetItem,QHeaderView
 from PyQt6.uic import loadUi
+from PyQt6 import QtCore, QtGui, QtWidgets
+class Ui_LoginScreen(object):
+    def setupUi(self, LoginScreen):
+        LoginScreen.setObjectName("LoginScreen")
+        LoginScreen.resize(1092, 774)
+        LoginScreen.setAutoFillBackground(False)
+        LoginScreen.setStyleSheet("background: #F8F8F8; ")
+        self.centralwidget = QtWidgets.QWidget(parent=LoginScreen)
+        self.centralwidget.setGeometry(QtCore.QRect(0, 0, 1092, 821))
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(parent=self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(490, 390, 111, 41))
+        self.pushButton.setStyleSheet("border-radius: 30px;\n"
+"border: 2px solid #665720;\n"
+"background: #AC9956;")
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(170, 70, 771, 91))
+        font = QtGui.QFont()
+        font.setFamily("Inter")
+        font.setPointSize(-1)
+        font.setBold(False)
+        font.setItalic(False)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: #6D6F11;\n"
+"text-align: center;\n"
+"font-family: Inter;\n"
+"font-size: 64px;\n"
+"font-style: normal;\n"
+"font-weight: 400;\n"
+"line-height: normal;")
+        self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.label.setObjectName("label")
+        self.textEdit = QtWidgets.QTextEdit(parent=self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(160, 270, 801, 61))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.textEdit.setFont(font)
+        self.textEdit.setAutoFillBackground(False)
+        self.textEdit.setStyleSheet("border-radius: 30px;\n"
+"border: 3px solid #AAA;\n"
+"background: #D9D9D9;")
+        self.textEdit.setLineWidth(2)
+        self.textEdit.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.SizeAdjustPolicy.AdjustToContents)
+        self.textEdit.setTabChangesFocus(True)
+        self.textEdit.setObjectName("textEdit")
+        self.label_2 = QtWidgets.QLabel(parent=self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(-10, 310, 571, 441))
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("screens\\../assets/butterfly_guy.jpg"))
+        self.label_2.setObjectName("label_2")
+        self.label_2.raise_()
+        self.pushButton.raise_()
+        self.label.raise_()
+        self.textEdit.raise_()
+        self.menubar = QtWidgets.QMenuBar(parent=LoginScreen)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1092, 22))
+        self.menubar.setObjectName("menubar")
+        self.statusbar = QtWidgets.QStatusBar(parent=LoginScreen)
+        self.statusbar.setGeometry(QtCore.QRect(0, 0, 3, 21))
+        self.statusbar.setObjectName("statusbar")
+
+        self.retranslateUi(LoginScreen)
+        QtCore.QMetaObject.connectSlotsByName(LoginScreen)
+
+    def retranslateUi(self, LoginScreen):
+        _translate = QtCore.QCoreApplication.translate
+        LoginScreen.setWindowTitle(_translate("LoginScreen", "LoginScreen"))
+        self.pushButton.setText(_translate("LoginScreen", "Sign In"))
+        self.label.setText(_translate("LoginScreen", "Please enter your User ID"))
+        self.textEdit.setHtml(_translate("LoginScreen", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:\'Segoe UI\'; font-size:20pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.textEdit.setPlaceholderText(_translate("LoginScreen", "Username"))
+class SEARCH_BUSINESS_FILTER:
+    def __init__(self, variant, value):
+        allowed_variant = ['MIN_STAR', "CITY", "NAME"]
+        if variant not in allowed_variant:
+            raise ValueError(f"The variant is not in {allowed_variant}")
+        self.variant = variant
+        self.value = value
+
+class SEARCH_BUSINESS_ORDER:
+    def __init__(self, variant):
+        allowed_variant = ['NO_OF_STARS', "CITY", "NAME"]
+        if variant not in allowed_variant:
+            raise ValueError(f"The variant is not in {allowed_variant}")
+        
+        self.variant = variant
+
+        match self.variant:
+            case "NO_OF_STARS":
+                self.value = "stars"
+            case "CITY":
+                self.value = "city"
+            case "NAME":
+                self.value = "name"
+class SEARCH_USER_YELP:
+    def __init__(self, variant, value):
+        allowed_variant = ['MIN_REVIEW_COUNT', "MIN_AVG_STAR", "NAME"]
+        if variant not in allowed_variant:
+            raise ValueError(f"The variant is not in {allowed_variant}")
+        
+        self.variant = variant
+
+        match self.variant:
+            case "MIN_REVIEW_COUNT":
+                if not (isinstance(value, float) or isinstance(value, int)):
+                    raise ValueError(f"MIN_REVIEW_COUNT must be float or int")
+            case "MIN_AVG_STAR":
+                if not (isinstance(value, float) or isinstance(value, int)):
+                    raise ValueError(f"MIN_REVIEW_COUNT must be float or int")
+            case "NAME":
+                if not isinstance(value, str):
+                    raise ValueError(f"MIN_REVIEW_COUNT must be str")
+        self.value = value
+class Functionality:
+    def __init__(self) -> None:
+        self.cursor = None
+        self.connection = None
+
+    def cursorCheck(self):
+        if self.cursor == None:
+            raise ValueError("Cursor have not exists yet")
+
+    def setCursor(self, cursor):
+        self.cursor = cursor
+    
+    def setConnection(self, connection):
+        self.connection = connection
+
+    def init_connection(self, db_host, db_name, db_user, db_password):
+        
+        connection_string = 'Driver={SQL Server};Server=' + db_host + ';Database=' + db_name + ';UID=' + db_user + ';PWD=' + db_password + ';'
+        # Establish a connection
+        connection = pypyodbc.connect(connection_string)
+               
+        return connection.cursor()
+
+    def login(self, user_id) -> None:
+        '''
+        Does it log the current user???
+        '''
+        self.cursorCheck()
+        self.cursor.execute(f"SELECT * FROM user_yelp WHERE user_yelp.user_id = '{user_id}'")
+
+        if len(self.cursor.fetchall()) != 0:
+            return True
+        
+        return False
+
+    def search_business(self, filter:SEARCH_BUSINESS_FILTER, orders: SEARCH_BUSINESS_ORDER) -> str:
+        '''
+        Return the business the user search, 
+        if the result is empty the application should handle that
+
+        When search for the name do we search match anything after the string or do we 
+        search for a string that contains the specific str
+
+        '''
+        self.cursorCheck()
+        
+        search = "SELECT business.business_id, business.name, business.address, business.city, business.stars FROM business"
+
+        search += " "
+
+        match filter.variant:
+            case "MIN_STAR":
+                search += f"WHERE business.stars >= {filter.value}"
+            case "CITY":
+                search += f"WHERE business.city = {filter.value}"
+            case "NAME":
+                search += f"WHERE business.name LIKE '{filter.value}%'"
+
+        search += " "
+        
+        match orders.variant:
+            case "NO_OF_STARS":
+                search += f"ORDER BY business.{orders.value}"
+            case "CITY":
+                search += f"ORDER BY business.{orders.value}"
+            case "NAME":
+                search += f"ORDER BY business.{orders.value}"
+        
+        self.cursor.execute(search)
+
+        return self.cursor.fetchall()
+
+    def search_users(self, filter:SEARCH_USER_YELP):
+        self.cursorCheck()
+        
+        search = "SELECT u.user_id, u.name, u.review_count, u.useful, u.funny, u.cool, u.average_stars, u.yelping_since FROM user_yelp u"
+        search += " "
+
+        match filter.variant:
+            case "MIN_REVIEW_COUNT":
+                search += f"WHERE u.review_count >= {filter.value}"
+            case "MIN_AVG_STAR":
+                search += f"WHERE u.average_stars >= {filter.value}"
+            case "NAME":
+                search += f"WHERE u.name like '{filter.value}%'"
+
+        self.cursor.execute(search)
+        return self.cursor.fetchall()
+
+    def make_friend(self, user1, user2) -> bool:
+        self.cursorCheck()
+
+        '''
+        Add two user to a friend ship table. Should check if the friend ship already exists
+        '''
+        self.cursor.execute(f"SELECT * FROM friendship WHERE user_id = '{user1}' AND friend = '{user2}'")
+        row = self.cursor.fetchall()
+
+        if len(row) != 0:
+            return False
+
+        self.cursor.execute(f"INSERT INTO friendship VALUES('{user1}', '{user2}')")
+        self.connection.commit()
+        return True
+
+    
+    def review_business(self,user_id, business_id, star):
+        self.cursorCheck()
+        import string
+        import random
+        
+        # initializing size of string
+        N = 22
+        
+        # using random.choices()
+        # generating random strings
+        random_id = ''.join(random.choices(string.ascii_lowercase +
+                                    string.digits, k=N))
+        command = f"INSERT INTO review (review_id, user_id, business_id, stars) VALUES ('{random_id}', '{user_id}', '{business_id}', {star});"
+        self.cursor.execute(command)
+        self.connection.commit()
+        
 
 class SearchBusinessScreen(QDialog):
     def __init__(self):
